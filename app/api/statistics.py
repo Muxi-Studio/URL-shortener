f252b416acdeb082c6a1cc1a33d0a42c9fcca0c0
Andrewpqc <m13636038496@163.com>
2018-06-11 01:11:56 +0800

    feat: change the permission control

--- app/api/statistics.py
+++ app/api/statistics.py
@@ -2,6 +2,7 @@ from . import api
 from app.models import Permission
 from flask_restful import Resource,abort,reqparse,Api
 from app.decorators import admin_required,permission_required
+
 api=Api(api,prefix="/statistics")
 
 class StatisticsHandlerClass(Resource):
