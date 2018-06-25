c64dffc24f3ef32d735c2e11dd652c0866a49a47
Andrewpqc <m13636038496@163.com>
2018-06-25 09:59:52 +0800

    feat: add pagenation support for statistics

--- app/api/statistics.py
+++ app/api/statistics.py
@@ -5,22 +5,35 @@ statistics.py URLmap的统计信息
 
 
 """
+import json
 from . import api
-from flask import jsonify
+from flask import jsonify,request
+from utils import pagination
 from .authentication import auth
-from app.models import Permission,URLMapping,Statistics
-from app.decorators import admin_required,permission_required,moderator_required
+from app.models import URLMapping,Statistics
+from app.decorators import moderator_required
 
 @auth.login_required
 @api.route("/urlmap/<int:id>/statistics/",methods=['GET'])
 def get_statistics_by_urlmapID(id):
     urlmap=URLMapping.query.get_or_404(id)
     statistics=urlmap.statistics.all()
-    return jsonify([s.to_json() for s in statistics])
+    return jsonify({"data":[s.to_json() for s in statistics]})
 
 @moderator_required
 @api.route('/statistics/',methods=['GET'])
 def get_all_statistics():
+    page = request.args.get('page', 1, type=int) or '1'
+    per_page = request.args.get('per_page', type=int) or '20'
     statistics=Statistics.query.all()
-    return jsonify([s.to_json() for s in statistics])
+    pagination_lit=pagination(statistics,int(page),int(per_page))
+    current = pagination_lit[0]
+    next_page = pagination_lit[1][0]
+    last_page = pagination_lit[1][1]
+    return json.dumps(
+        [s.to_json() for s in current],
+        ensure_ascii=False,
+        indent=1
+    ), 200, {'link': '<%s>; rel="next", <%s>; rel="last"' %
+                     (next_page, last_page)}
 
